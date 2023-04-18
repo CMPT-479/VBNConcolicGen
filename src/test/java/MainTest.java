2e42403e19d3b4064b6a58e514a0ab4e66ad0740
Nathan Tsai <nathan@albernidesign.com>
2023-04-18 14:21:44 -0700

    Replace Soot's Class Path to support both Windows & Unix platforms.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,12 +5,21 @@ import soot.G;
 import soot.PackManager;
 import soot.Transform;
 import soot.options.Options;
+import java.io.File;
 
 public class MainTest {
+
     @BeforeEach
     final void initializeSoot() {
         G.reset();
-        Options.v().set_soot_classpath(".:target/classes:VIRTUAL_FS_FOR_JDK");
+
+        final String[] paths = {
+                ".",
+                new File("target", "classes").toString(),
+                "VIRTUAL_FS_FOR_JDK",
+        };
+
+        Options.v().set_soot_classpath(String.join(File.pathSeparator, paths));
         final var cute = new CuteTransformer();
         final var transform = new Transform("jtp.CuteTransformer", cute);
         PackManager.v().getPack("jtp").add(transform);
