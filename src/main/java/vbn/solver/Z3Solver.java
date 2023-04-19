61c4e6d9e1ba61623089759d0701f6fcd09efa5f
Brendan Saw <brendansaw@gmail.com>
2023-04-19 11:21:53 -0700

    Added binary constraint handler

--- src/main/java/vbn/solver/Z3Solver.java
+++ src/main/java/vbn/solver/Z3Solver.java
@@ -1,106 +1,144 @@
 package vbn.solver;
 
 import com.microsoft.z3.*;
-import vbn.constraints.Constraint;
-//import vbn.constraints.ConstraintIntComp;
-import vbn.constraints.State;
+import lombok.NonNull;
+import soot.util.Cons;
+import vbn.constraints.*;
 import vbn.constraints.Symbol;
+//import vbn.constraints.ConstraintIntComp;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import javax.annotation.Nullable;
+import java.util.*;
 
 public class Z3Solver {
 
 
-    public static BoolExpr handleBoolConstraints(ConstraintItemBool constraintItemBool,
-                                                 Map<String, Expr> z3ExprMap, Context ctx) {
-        BoolExpr expr = null;
-        if (constraintItemBool == null ||
-                constraintItemBool.left == null || constraintItemBool.left.id == null ||
-                constraintItemBool.right == null || constraintItemBool.right.id == null) {
-            return null;
-        }
+    public static Expr handleBinaryConstraints(@NonNull Context ctx, @NonNull Map<String, Expr> z3ExprMap,
+                                               @NonNull BinaryConstraint binaryConstraint) {
+        @NonNull Symbol leftSymbol = binaryConstraint.left;
+        @NonNull Symbol rightSymbol = binaryConstraint.right;
+        @NonNull BinaryOperand op = binaryConstraint.op;
+        @Nullable Symbol assigned = binaryConstraint.assigned;  // optional
 
-        Expr leftExpr = z3ExprMap.get(constraintItemBool.left.id);
-        Expr rightExpr = z3ExprMap.get(constraintItemBool.right.id);
-        if (leftExpr == null || rightExpr == null) {
-            return null;
-        }
+        @NonNull Expr leftExpr = z3ExprMap.get(leftSymbol.id);
+        @NonNull Expr rightExpr = z3ExprMap.get(rightSymbol.id);
 
-        switch (constraintItemBool.op) {
-            case OR:
-                expr = ctx.mkOr(leftExpr, rightExpr);
-                break;
+        Expr exprToReturn = null;
+        switch (op) {
             case AND:
-                expr = ctx.mkAnd(leftExpr, rightExpr);
+                exprToReturn = ctx.mkAnd(leftExpr, rightExpr);
+                break;
+            case OR:
+                exprToReturn = ctx.mkOr(leftExpr, rightExpr);
                 break;
             case EQ:
-                expr = ctx.mkEq(leftExpr, rightExpr);
+                exprToReturn = ctx.mkEq(leftExpr, rightExpr);
+                break;
+            case ADD:
+                exprToReturn = ctx.mkAdd(leftExpr, rightExpr);
+                break;
+            case MINUS:
+                exprToReturn = ctx.mkSub(leftExpr, rightExpr);
+                break;
+            case MULTIPLY:
+                exprToReturn = ctx.mkMul(leftExpr, rightExpr);
+                break;
+            case DIVIDE:
+                exprToReturn = ctx.mkDiv(leftExpr, rightExpr);
+                break;
+            case POWER:
+                exprToReturn = ctx.mkPower(leftExpr, rightExpr);
+                break;
+            case INT_LT:
+                exprToReturn = ctx.mkLt(leftExpr, rightExpr);
+                break;
+            case INT_LTE:
+                exprToReturn = ctx.mkLe(leftExpr, rightExpr);
+                break;
+            case INT_EQ:
+                exprToReturn = ctx.mkEq(leftExpr, rightExpr);
+                break;
+            case INT_NEQ:
+                exprToReturn = ctx.mkNot(ctx.mkEq(leftExpr, rightExpr));
+                break;
+            case INT_GT:
+                exprToReturn = ctx.mkGt(leftExpr, rightExpr);
+                break;
+            case INT_GTE:
+                exprToReturn = ctx.mkGe(leftExpr, rightExpr);
                 break;
         }
-        return expr;
+        if (exprToReturn != null && assigned != null) {
+            @NonNull Expr assignedExpr = z3ExprMap.get(assigned.id);
+            exprToReturn = ctx.mkEq(assignedExpr, exprToReturn);
+        }
+
+        return exprToReturn;
     }
 
-    public static IntExpr handleIntConstraints(ConstraintItemInt constraintItemInt,
-                                               Map<String, Expr> z3ExprMap, Context ctx) {
-        IntExpr expr = null;
-        if (constraintItemInt == null ||
-                constraintItemInt.left == null || constraintItemInt.left.id == null ||
-                constraintItemInt.right == null || constraintItemInt.right.id == null) {
-            return null;
-        }
+//
+//    public static IntExpr handleIntConstraints(ConstraintItemInt constraintItemInt,
+//                                               Map<String, Expr> z3ExprMap, Context ctx) {
+//        IntExpr expr = null;
+//        if (constraintItemInt == null ||
+//                constraintItemInt.left == null || constraintItemInt.left.id == null ||
+//                constraintItemInt.right == null || constraintItemInt.right.id == null) {
+//            return null;
+//        }
+//
+//        Expr leftExpr = z3ExprMap.get(constraintItemInt.left.id);
+//        Expr rightExpr = z3ExprMap.get(constraintItemInt.right.id);
+//
+//        if (leftExpr == null || rightExpr == null) {
+//            return null;
+//        }
+//
+//        switch (constraintItemInt.op) {
+//            case EQ:
+//                break;
+//            case GE:
+//                break;
+//            case GT:
+//                break;
+//            case LE:
+//                break;
+//            case LT:
+//                break;
+//        }
+//        return expr;
+//    }
 
-        Expr leftExpr = z3ExprMap.get(constraintItemInt.left.id);
-        Expr rightExpr = z3ExprMap.get(constraintItemInt.right.id);
-
-        if (leftExpr == null || rightExpr == null) {
-            return null;
-        }
-
-        switch (constraintItemInt.op) {
-            case EQ:
-                break;
-            case GE:
-                break;
-            case GT:
-                break;
-            case LE:
-                break;
-            case LT:
-                break;
-        }
-        return expr;
-    }
-
-    public static void solve(State state) {
+    public static void solve(@NonNull State state) {
         System.out.println("================ TESTING Z3 SOLVER ================");
         Context ctx = new Context();
         Solver solver = ctx.mkSolver();
 
-        Map<String, vbn.constraints.Symbol> symbolMap = state.getSymbolsMap();
-        Set<String> keys = state.getSymbolsMap().keySet();
+        Collection<Symbol> symbols = state.getSymbols();
         Map<String, Expr> z3ExprMap = new HashMap<>();
-        for (String k : keys) {
-            switch (symbolMap.get(k).symbolType) {
+        for (var sym : symbols) {
+            switch (sym.symbolType) {
                 case INT_TYPE:
-                    z3ExprMap.put(k, ctx.mkIntConst(k));
+                    z3ExprMap.put(sym.id, ctx.mkIntConst(sym.id));
                     break;
                 case BOOL_TYPE:
-                    z3ExprMap.put(k, ctx.mkBoolConst(k));
+                    z3ExprMap.put(sym.id, ctx.mkBoolConst(sym.id));
                     break;
                 default:
+                    System.out.println("ERROR, Symbol type wasn't of expected type");
                     break;
             }
         }
-        Set<Expr> constraints = new HashSet<>();
-        for (ConstraintItem constraintItem : state.getConstraints()) {
-            System.out.println(constraintItem.getClass());
-            if (constraintItem.getClass().equals(ConstraintItemBool.class)) {
-                constraints.add(handleBoolConstraints((ConstraintItemBool) constraintItem, z3ExprMap, ctx));
-            } else if (constraintItem.getClass().equals(ConstraintItemInt.class)) {
-                constraints.add(handleIntConstraints((ConstraintItemInt) constraintItem, z3ExprMap, ctx));
+
+        Stack<Constraint> constraintStack = state.getConstraints();
+        // will need to keep negating the top of the stack and then removing it while going down
+        for (Constraint constraint : constraintStack) {
+            System.out.println(constraint.getClass());
+            if (constraint.getClass().equals(UnaryConstraint.class)) {
+                solver.add();
+//                constraints.add(handleBoolConstraints((ConstraintItemBool) constraintItem, z3ExprMap, ctx));
+            } else if (constraint.getClass().equals(BinaryConstraint.class)) {
+                solver.add(handleBinaryConstraints(ctx, z3ExprMap, (BinaryConstraint) constraint));
+//                constraints.add(handleIntConstraints((ConstraintItemInt) constraintItem, z3ExprMap, ctx));
             } else {
                 System.out.println("Error");
             }
@@ -116,9 +154,9 @@ public class Z3Solver {
 //        BoolExpr pathConstraint =;
 
         // Add path constraint to solver
-        for (Expr expr : constraints) {
-            solver.add(expr);
-        }
+//        for (Expr expr : constraints) {
+//            solver.add(expr);
+//        }
 //        solver.add(pathConstraint);
 
         // Check for satisfying assignment
@@ -126,9 +164,9 @@ public class Z3Solver {
         if (status == Status.SATISFIABLE) {
             // Get satisfying assignment
             Model model = solver.getModel();
-            for (String k : keys) {
-                System.out.println(k + " = " + model.eval(z3ExprMap.get(k), true));
-            }
+//            for (String k : keys) {
+//                System.out.println(k + " = " + model.eval(z3ExprMap.get(k), true));
+//            }
 //            System.out.println("x = " + model.eval(x, true));
 //            System.out.println("y = " + model.eval(y, true));
         } else if (status == Status.UNSATISFIABLE) {
