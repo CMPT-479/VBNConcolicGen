8642f144e5cbd60d965286783e30667e6b3cf533
Brendan Saw <brendansaw@gmail.com>
2023-04-20 13:11:05 -0700

    Minor changes, focusing on adding a few more comments for future actions to be taken

--- src/main/java/vbn/solver/VBNRunner.java
+++ src/main/java/vbn/solver/VBNRunner.java
@@ -15,11 +15,13 @@ public class VBNRunner {
         final String[] args = new String[] {programName};
         soot.Main.main(args);
         InstrumentedRunner.runInstrumented(programName, programInputs);
+
+        Stack<Constraint> stateConstraint;
         while (true) {
             // this global state needs to be obtained from an external data store
             State state = new State(); // shouldn't be a "new"
-            ArrayList<AbstractSymbolConstant> solved = Z3Solver.solve(state);
-            Stack<Constraint> stateConstraint = state.getConstraints();
+            ArrayList<AbstractSymbolConstant> solved = Z3Solver.solve(state);   // need to save these solved values somewhere
+            stateConstraint = state.getConstraints();
             while (!(stateConstraint.empty()) && (stateConstraint.peek().negated)) {
                 stateConstraint.pop();
             }
@@ -27,10 +29,6 @@ public class VBNRunner {
                 break;
             }
             stateConstraint.peek().negated = true;
-
-            // negate last item in stack or pop if negated last item already, then negate
-            // while ()
-
             InstrumentedRunner.runInstrumented(programName, programInputs);
         }
     }
