738857d876eb665c29d1db4c6b4f110f59a899a9
Brendan Saw <brendansaw@gmail.com>
2023-04-20 10:02:53 -0700

    Added equals function within constraints and symbol to allow for easier comparison of stacks

--- src/main/java/vbn/constraints/UnaryConstraint.java
+++ src/main/java/vbn/constraints/UnaryConstraint.java
@@ -1,22 +1,45 @@
 package vbn.constraints;
 
+import lombok.NonNull;
 import vbn.constraints.Value.Symbol;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
 
 public class UnaryConstraint extends Constraint {
 
-    public Symbol assigned;
-    public UnaryOperand op;
-    public Symbol symbol;
+    @Nullable public Symbol assigned;
+    @NonNull public UnaryOperand op;
+    @NonNull public Symbol symbol;
 
-    public UnaryConstraint(UnaryOperand op, Symbol symbol) {
+    public UnaryConstraint(@NonNull UnaryOperand op, @NonNull Symbol symbol) {
         this.assigned = null;
         this.symbol = symbol;
         this.op = op;
     }
-    public UnaryConstraint(UnaryOperand op, Symbol symbol, Symbol assigned) {
+    public UnaryConstraint(@NonNull UnaryOperand op, @NonNull Symbol symbol, Symbol assigned) {
         this.assigned = assigned;
         this.symbol = symbol;
         this.op = op;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof UnaryConstraint)) {
+            return false;
+        }
+
+        UnaryConstraint otherUnaryConstraint = (UnaryConstraint) obj;
+        if (this.assigned == null || otherUnaryConstraint.assigned == null) {
+            if (this.assigned != otherUnaryConstraint.assigned) {
+                return false;
+            }
+            return Objects.equals(this.symbol, otherUnaryConstraint.symbol)
+                    && this.op == otherUnaryConstraint.op;
+        } else {
+            return Objects.equals(this.symbol, otherUnaryConstraint.symbol)
+                    && Objects.equals(this.assigned, otherUnaryConstraint.assigned)
+                    && this.op == otherUnaryConstraint.op;
+        }
+    }
 }
