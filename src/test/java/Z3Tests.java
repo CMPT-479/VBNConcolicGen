9030ad464ef0b4588f2e821e3a60941b2de7b981
Nathan Tsai <nathan@albernidesign.com>
2023-04-20 20:19:24 -0700

    Refactor Main Test file

---
+++ src/test/java/Z3Tests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import vbn.solver.Z3Solver;
+import vbn.state.State;
+import vbn.state.constraints.BinaryConstraint;
+import vbn.state.constraints.BinaryOperand;
+import vbn.state.value.AbstractSymbol;
+import vbn.state.value.BooleanSymbol;
+import vbn.state.value.IntSymbol;
+
+import java.util.List;
+
+public class Z3Tests {
+    @Test
+    final void testZ3SimpleOr() {
+        // x or y
+        State state = new State();
+        state.addSymbol(new BooleanSymbol("x", false));  // concrete value doesn't matter
+        state.addSymbol(new BooleanSymbol("y", false));  // concrete value doesn't matter
+        state.pushConstraint(
+                new BinaryConstraint(
+                        state.getSymbol("x"),
+                        BinaryOperand.OR,
+                        state.getSymbol("y")));
+
+        List<AbstractSymbol> solved = Z3Solver.solve(state);
+        Z3Solver.printSolvedValuesBasedOnList(solved);
+    }
+
+    @Test
+    final void testZ3SimpleAnd() {
+        // x and y
+        State state = new State();
+        state.addSymbol(new BooleanSymbol("x", false));  // concrete value doesn't matter
+        state.addSymbol(new BooleanSymbol("y", false));  // concrete value doesn't matter
+        state.pushConstraint(
+                new BinaryConstraint(
+                        state.getSymbol("x"),
+                        BinaryOperand.AND,
+                        state.getSymbol("y")));
+
+        List<AbstractSymbol> solved = Z3Solver.solve(state);
+        Z3Solver.printSolvedValuesBasedOnList(solved);
+    }
+
+    @Test
+    final void testZ3Gt() {
+        // x > y
+        State state = new State();
+        state.addSymbol(new IntSymbol("x", 1));  // concrete value doesn't matter
+        state.addSymbol(new IntSymbol("y", 1));  // concrete value doesn't matter
+        state.pushConstraint(
+                new BinaryConstraint(
+                        state.getSymbol("x"),
+                        BinaryOperand.GT,
+                        state.getSymbol("y")));
+
+        List<AbstractSymbol> solved = Z3Solver.solve(state);
+        Z3Solver.printSolvedValuesBasedOnList(solved);
+    }
+}
