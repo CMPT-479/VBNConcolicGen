74d359ae5f85bd6f75603db02fe420e7f06e23e9
Viet Hung Nguyen <vhn@sfu.ca>
2023-04-20 17:14:51 -0700

    Wrap main with try catch

--- src/main/java/vbn/instrument/Instrument.java
+++ src/main/java/vbn/instrument/Instrument.java
@@ -2,6 +2,7 @@ package vbn.instrument;
 
 import soot.jimple.Jimple;
 import soot.jimple.Stmt;
+import soot.jimple.internal.JCaughtExceptionRef;
 import soot.tagkit.LineNumberTag;
 import vbn.instrument.switches.StatementSwitch;
 import soot.*;
@@ -46,11 +47,29 @@ public class Instrument extends BodyTransformer {
             body.getUnits().addFirst(stmt);
         }
 
+        wrapInsideTryCatch(body);
+
         it = units.snapshotIterator();
         System.out.println("---after instrument");
         while (it.hasNext()) {
             var unit = it.next();
             System.out.println("\t" + unit);
         }
+    }
+
+    private void wrapInsideTryCatch(Body body) {
+        if (!body.getMethod().getSubSignature().equals("void main(java.lang.String[])")) return;
+        var units = body.getUnits();
+        var exception = Jimple.v().newLocal("exception", RefType.v("java.lang.Throwable"));
+        body.getLocals().add(exception);
+        var eStmt = Jimple.v().newIdentityStmt(exception, new JCaughtExceptionRef());
+        var trap = Jimple.v().newTrap(Scene.v().getSootClass("java.lang.Throwable"),
+                units.getFirst(), units.getLast(), eStmt);
+        body.getTraps().addLast(trap);
+        body.getUnits().addLast(eStmt);
+        var errorMethod = runtime.getMethod("void error()").makeRef();
+        var errorCall = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(errorMethod));
+        body.getUnits().addLast(errorCall);
+        body.getUnits().addLast(Jimple.v().newReturnVoidStmt());
     }
 }
