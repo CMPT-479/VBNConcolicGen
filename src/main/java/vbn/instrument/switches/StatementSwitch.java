fe896ed5989991643268b2967dee5ec74000446b
Viet Hung Nguyen <vhn@sfu.ca>
2023-04-21 13:27:52 -0700

    Add finalizeIf

--- src/main/java/vbn/instrument/switches/StatementSwitch.java
+++ src/main/java/vbn/instrument/switches/StatementSwitch.java
@@ -39,15 +39,16 @@ public class StatementSwitch extends AbstractStmtSwitch<Object> {
         JimpleValueInstrument.instrument(condition, null, stmt, data);
         var trueBranch = data.runtime.getMethod("void pushTrueBranch(int)").makeRef();
         var falseBranch = data.runtime.getMethod("void pushFalseBranch(int)").makeRef();
+        var finalizeIf = data.runtime.getMethod("void finalizeIf(int)").makeRef();
         var lineNumber = IntConstant.v(((LineNumberTag) stmt.getTag("LineNumberTag")).getLineNumber());
         var trueStmt = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(trueBranch, lineNumber));
         var falseStmt = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(falseBranch, lineNumber));
         var ifStmt = Jimple.v().newIfStmt(condition, trueStmt);
-        var noOpStmt = Jimple.v().newNopStmt();
+        var finalizeStmt = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(finalizeIf, lineNumber));
         List<Unit> units = List.of(
                 ifStmt, falseStmt,
-                Jimple.v().newGotoStmt(noOpStmt),
-                trueStmt, noOpStmt
+                Jimple.v().newGotoStmt(finalizeStmt),
+                trueStmt, finalizeStmt
         );
         data.units.insertBefore(units, stmt);
     }
