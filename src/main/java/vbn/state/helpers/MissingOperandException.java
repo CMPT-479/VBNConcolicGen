a7b192a34de6cf6337f6339f938bbec36395e0bc
Nathan Tsai <nathan@albernidesign.com>
2023-04-21 16:40:24 -0700

    Set value of if conditions

--- src/main/java/vbn/state/helpers/MissingOperandException.java
+++
@@ -1,9 +0,0 @@
-package vbn.state.helpers;
-
-public class MissingOperandException extends ComputeException {
-    private static final long serialVersionUID = 0L;
-
-    public MissingOperandException(String errorMessage) {
-        super(errorMessage);
-    }
-}
