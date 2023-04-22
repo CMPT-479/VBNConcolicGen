f4115fb33dd21cd8b39b0e64c4955eea1d3f7ef7
Brendan Saw <brendansaw@gmail.com>
2023-04-21 21:37:59 -0700

    Added Koushik Sen's tests and ensured that they ran as intended

--- src/test/java/KSenTests.java
+++ src/test/java/KSenTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import soot.G;
 import soot.PackManager;
 import soot.Transform;
@@ -7,6 +8,8 @@ import vbn.instrument.Instrument;
 import vbn.solver.VBNRunner;
 
 import java.io.File;
+
+import static vbn.solver.VBNRunner.printAndValidateSolvedConstraints;
 
 public class KSenTests {
     @BeforeEach
@@ -26,5 +29,22 @@ public class KSenTests {
         VBNRunner.reset();
     }
 
+    @Test
+    final void ksenIf() {
+        VBNRunner.execute("vbn.ksen_tests.If");
+        printAndValidateSolvedConstraints(2);
+    }
+
+    @Test
+    final void ksenLinear() {
+        VBNRunner.execute("vbn.ksen_tests.Linear");
+        printAndValidateSolvedConstraints(3);
+    }
+
+    @Test
+    final void ksenNonlinear() {
+        VBNRunner.execute("vbn.ksen_tests.Nonlinear");
+        printAndValidateSolvedConstraints(3);
+    }
 
 }
