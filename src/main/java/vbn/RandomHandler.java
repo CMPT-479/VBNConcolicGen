0c9fc86212a4ca1a40c7fffcbec8107620ecc207
Brendan Saw <brendansaw@gmail.com>
2023-04-21 20:23:11 -0700

    Incorporating changes from @chrishappy #71 PR

--- src/main/java/vbn/RandomHandler.java
+++ src/main/java/vbn/RandomHandler.java
@@ -3,17 +3,23 @@ package vbn;
 import java.util.Random;
 
 public class RandomHandler {
-    static Random random = new Random(System.currentTimeMillis());
+    static long seed;
+    static Random random;
 
-    public static int getRandomNumber() {
+    public RandomHandler(long seed) {
+        this.seed = seed;
+        this.random = new Random(this.seed);
+    }
+
+    public int getRandomNumber() {
         return random.nextInt();
     }
 
-    public static boolean getRandomBoolean() {
+    public boolean getRandomBoolean() {
         return random.nextBoolean();
     }
 
-    public static double getRandomReal() {
+    public double getRandomReal() {
         return random.nextDouble();
     }
 }
