abf41eea312738d15dc702b0f6286d63b9a6b60d
Brendan Saw <brendansaw@gmail.com>
2023-04-21 20:51:50 -0700

    Modified the inputs for test 2

--- src/main/java/vbn/examples/Test_02_NEG_vs_MINUS.java
+++ src/main/java/vbn/examples/Test_02_NEG_vs_MINUS.java
@@ -1,10 +1,14 @@
 package vbn.examples;
 
 public class Test_02_NEG_vs_MINUS {
+
+    static int x;
+    static int y;
+    static boolean z;
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[1]);
-        int y = Integer.parseInt(args[2]);
-        boolean z = Boolean.parseBoolean(args[3]);
+        x = Integer.parseInt(args[0]);
+        y = Integer.parseInt(args[1]);
+        z = Boolean.parseBoolean(args[2]);
 
         // Negation is handled through a if statement
         boolean not_z = !z;
