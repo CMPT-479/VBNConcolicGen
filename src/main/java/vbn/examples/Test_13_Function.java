fe501d3c354769ba8923b861fbf6542abbd36c92
Brendan Saw <brendansaw@gmail.com>
2023-04-23 11:15:27 -0700

    Made changes to test cases to better evaluate the program behaviour

--- src/main/java/vbn/examples/Test_13_Function.java
+++ src/main/java/vbn/examples/Test_13_Function.java
@@ -2,10 +2,6 @@ package vbn.examples;
 
 public class Test_13_Function {
 
-    static int a;
-    static int b;
-    static int c;
-
     public static int f(int x, int y) {
         if (x < y) {
             return x + y;
@@ -14,11 +10,13 @@ public class Test_13_Function {
             return x - y;
         }
     }
+    static int x;
 
     public static void main(String[] args) {
-        a = f(2 ,3); // 2^1
-        b = f(a, 4);    // 2^2
-        c = f(f(2, a), b); // 2^4
+        x = Integer.parseInt(args[0]);
+        int a = f(x ,3);        // 2^1
+        int b = f(a, 4);        // 2^2
+        int c = f(f(2, a), b);  // 2^4
         System.out.println(c);
     }
 }
